aceb6f8838eece7b608d9addfa368f1e67156544
Yannick Kirschen <yannickkirschen@protonmail.com>
2023-09-12 13:32:01 +0200

    fix: exit when run.sh not run as sudo

--- digital-rail-concentrator/assets/run.sh
+++ digital-rail-concentrator/assets/run.sh
@@ -2,6 +2,7 @@
 
 if [[ "$EUID" != 0 ]]; then
     echo "Pi4J needs to be run as root. Try using sudo ./run.sh"
+    exit 1
 fi
 
 java -classpath "$(dirname "$(realpath "$0")")/*" sh.yannick.rail.concentrator.Main "$@"
